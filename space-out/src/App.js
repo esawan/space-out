7e18479325085f1ecc51083533fbe32c2b88b3cc
kckc1289 <keziah.camille8@ymail.com>
2020-04-26 20:47:05 -0700

    Almost done with routing

--- space-out/src/App.js
+++ space-out/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import HomePage from "./components/HomePage/HomePage";
 import Kids from "./Kids";
+
+/**
+ * Need to run this on command line to get router to work on page; 
+ * npm install -S react-router-dom
+ * or this idk
+ * npm install react-router-dom --save
+ */
 
 function App() {
   return (
@@ -10,8 +19,9 @@ function App() {
         S P A C E  O U T 
       </header>
 
+      <Router>
       <div className="HomeTab">
-        Home
+      <Link to="/" style={{textDecoration: 'none'}}> Home </Link>
       </div>
 
       <div className="KidsTab">
@@ -29,6 +39,8 @@ function App() {
       <div className="AboutTab">
         About
       </div>
+      <Route exact path = "/" component = {HomePage}/>
+      </Router>
 
       <div className="Content">
          <p>Welcome to S P A C E  O U T</p> 
---
+++ space-out/src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+
+function HomePage() {
+    return (
+        <div>
+            <div className="Content">
+            <h1></h1>
+            </div>
+        </div>
+    );
+}
+export default HomePage;
