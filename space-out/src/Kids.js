911910375b7c005801e9937ade3cc46e06fb0104
stesfu <salemtesfu@gmail.com>
2020-04-26 20:29:36 -0500

    Added more links to the kids page

--- space-out/src/Kids.js
+++ space-out/src/Kids.js
@@ -14,7 +14,9 @@ class Kids extends Component {
   render() {
     return (
       <div>
-        <IframeComponent src="//cdn.witchhut.com/html5/games/color-me-princess/" height="600px" width="800px"/>
+        <IframeComponent src="https://www.youtube.com/embed/videoseries?list=PLrOK2BUgNUQO6A0Z3jYTkFITI5ZZ-v5Iz" height="600px" width="800px"/>
+        <IframeComponent src="//cdn.witchhut.com/html5/games/color-me-pets/" height="600px" width="800px"/>
+        <IframeComponent src="//cdn.witchhut.com/html5/games/trick-hoops-puzzle-edition/" height="600px" width="800px"/>
       </div>
     );
   }
