df605201c9f1e390177f434b5a4b74ed9e1f45cc
Elise <elise@Ifuekos-MacBook-Pro-2.local>
2020-04-28 00:42:10 -0700

    Finished styling, fixed home page routing issue, fixed content scrolling display issue

--- space-out/src/components/HomePage/HomePage.js
+++ space-out/src/components/HomePage/HomePage.js
@@ -4,8 +4,8 @@ import '../../App.css';
 function HomePage() {
     return (
         <div className = "Content">
-            <p>Welcome to S P A C E  O U T</p> 
-            <p>A place to relax :)</p>
+            <p>Welcome to SPACE OUT!</p> 
+            <p>It's a place to relax ~ click around and have a calming time</p>
             <p>Home page is currently under construction.</p> 
         </div>
     );
